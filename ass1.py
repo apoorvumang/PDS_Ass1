79716c47e69fddc4c63560456d3e5976f2a1096e
Apoorv Umang <apoorvumang@gmail.com>
2019-02-12 12:04:50 +0530

    did for larger l values

--- ass1.py
+++ ass1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import datetime
 epsilon = 0.00000001
 def isRowZero(row):
 	global epsilon
@@ -11,9 +12,10 @@ def isRowZero(row):
 	return True
 
 
-l = 100
+l = 250
+print(datetime.datetime.now())
 print "Loading data.."
-X = np.loadtxt('data/data.txt', delimiter=',')
+X = np.loadtxt('data/data_no_target.txt', delimiter=',')
 Y = np.zeros((l, X.shape[1]))
 d = X.shape[1]
 n = X.shape[0]
@@ -25,6 +27,7 @@ print "d = " + str(d)
 print "X is " + str(X.shape)
 print "Y is " + str(Y.shape)
 i = 0
+print(datetime.datetime.now())
 while i < n:
 	rowWasZero = False
 	# get first zero row
@@ -43,7 +46,11 @@ while i < n:
 	# matrix filled
 	# print ('Filled matrix. i = ' + str(i))
 	u, s, vh = np.linalg.svd(Y, full_matrices=True)
-	delta = (s[l/2])*(s[l/2])
+	delta = 0
+	if(l >= d*2):
+		delta = (s[d-1])*(s[d-1])
+	else:
+		delta = (s[l/2])*(s[l/2])
 	new_s = np.zeros((min(l,d)))
 	for j in range(min(l,d)):
 		temp = s[j]*s[j] - delta
@@ -61,3 +68,4 @@ xtx = np.matmul(X.T, X)
 norm = np.linalg.norm(xtx - yty)
 print "xtx - yty norm = \t" + str(norm)
 print "norm(X)^2/l = \t\t" + str(math.pow(np.linalg.norm(X), 2)/l)
+print(datetime.datetime.now())
